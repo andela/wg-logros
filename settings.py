ed54cd1064b65cb110d08fd396d297fe1e6e9d90
Shakira Seruwagi <shakiraseruwagi@Shakiras-MacBook-Pro.local>
2018-02-13 14:39:00 +0300

    [Chore #155124480] Changes to bower.json file

--- settings.py
+++ settings.py
@@ -69,5 +69,6 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'wger/core/static'),
 )
+
--- wger/settings_global.py
+++ wger/settings_global.py
@@ -106,8 +106,8 @@ BOWER_INSTALLED_APPS = (
 
 
 MIDDLEWARE_CLASSES = (
-    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
