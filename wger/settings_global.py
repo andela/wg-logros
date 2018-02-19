40fb0d46be1ef02756b464bf03317d21d2a2c14a
kevinsamoei <samoeikev@gmail.com>
2018-02-19 13:10:19 +0300

    [Bug #155322966] Fix social login bug: - Remove duplicate authentication backends - Update requirements_devel.txt
    [Delivers #155322966]

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -140,8 +140,6 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOpenId',
     'social_core.backends.google.GoogleOAuth2',
 )
-AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',
-                           'wger.utils.helpers.EmailAuthBackend')
 
 TEMPLATES = [
     {
