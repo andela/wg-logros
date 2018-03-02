99bdc28cb533bc4e1803278d2dfe58d506107fb3
hadijahkyampeire <hadijah315@gmail.com>
2018-03-02 15:43:13 +0300

    [ft-154953794]Add-macrocycle-mesocycle-microcycle-support: - enable periodization on editing
    [Delivers #154953794]

--- wger/manager/views/schedule_step.py
+++ wger/manager/views/schedule_step.py
@@ -20,13 +20,24 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy, ugettext as _
 from django.db import models
-from django.forms import ModelForm, ModelChoiceField
+from django import forms
 from django.views.generic import (CreateView, DeleteView, UpdateView)
+
+from wger.manager.helpers import (MACROCYCLE, MESOCYLCLE, MICROCYCLE, periodization)
 
 from wger.manager.models import (Schedule, ScheduleStep, Workout)
 from wger.utils.generic_views import (WgerFormMixin, WgerDeleteMixin)
 
 logger = logging.getLogger(__name__)
+
+PERIODIZATION_CHOICES = (
+    (periodization.get_max(MICROCYCLE),
+     '%s(1 week)' % MICROCYCLE.capitalize()),
+    (periodization.get_max(MESOCYLCLE),
+     '%s(2-6 weeks)' % MESOCYLCLE.capitalize()),
+    (periodization.get_max(MACROCYCLE),
+     '%s(1 year)' % MACROCYCLE.capitalize()),
+)
 
 
 class StepCreateView(WgerFormMixin, CreateView, PermissionRequiredMixin):
@@ -46,15 +57,37 @@ class StepCreateView(WgerFormMixin, CreateView, PermissionRequiredMixin):
         have we access to the current user
         '''
 
-        class StepForm(ModelForm):
-            workout = ModelChoiceField(
+        class StepForm(forms.ModelForm):
+            workout = forms.ModelChoiceField(
                 queryset=Workout.objects.filter(user=self.request.user))
 
             class Meta:
                 model = ScheduleStep
-                exclude = ('order', 'schedule')
+                exclude = ('order', 'schedule', 'is_periodized')
+
+            def __init__(self, *args, **kwargs):
+                if 'schedule_pk' in kwargs:
+                    self.schedule_pk = kwargs.pop('schedule_pk')
+
+                super(StepForm, self).__init__(*args, **kwargs)
+
+                if hasattr(self, 'schedule_pk'):
+                    schedule = Schedule.objects.get(pk=self.schedule_pk)
+
+                    if getattr(schedule, 'use_periodization'):
+                        del self.fields['duration']
+                        new_duration_field = forms.ChoiceField(choices=PERIODIZATION_CHOICES)
+                        self.fields['duration'] = new_duration_field
 
         return StepForm
+
+    def get_form_kwargs(self):
+        kwargs = super(StepCreateView, self).get_form_kwargs()
+
+        if 'schedule_pk' not in kwargs:
+            kwargs['schedule_pk'] = self.kwargs.get('schedule_pk')
+
+        return kwargs
 
     def get_context_data(self, **kwargs):
         context = super(StepCreateView, self).get_context_data(**kwargs)
@@ -69,8 +102,18 @@ class StepCreateView(WgerFormMixin, CreateView, PermissionRequiredMixin):
 
     def form_valid(self, form):
         '''Set the schedule and the order'''
-
+        uses_periodization = False
         schedule = Schedule.objects.get(pk=self.kwargs['schedule_pk'])
+
+        if schedule.use_periodization:
+            # mark schedule step as periodized plan.
+            form.instance.is_periodized = True
+            uses_periodization = True
+
+        if not uses_periodization and form.cleaned_data['duration'] > 25:
+            # normal scheduled steps should not exceed 25 weeks
+            form.add_error('duration', 'Ensure that duration value is equal or less than 25')
+            return super(StepCreateView, self).form_invalid(form)
 
         max_order = schedule.schedulestep_set.all().aggregate(
             models.Max('order'))
@@ -96,15 +139,61 @@ class StepEditView(WgerFormMixin, UpdateView, PermissionRequiredMixin):
         have we access to the current user
         '''
 
-        class StepForm(ModelForm):
-            workout = ModelChoiceField(
+        class StepForm(forms.ModelForm):
+            workout = forms.ModelChoiceField(
                 queryset=Workout.objects.filter(user=self.request.user))
 
             class Meta:
                 model = ScheduleStep
-                exclude = ('order', 'schedule')
+                exclude = ('order', 'schedule', 'is_periodized')
+
+            def __init__(self, *args, **kwargs):
+                if 'schedule_pk' in kwargs:
+                    self.schedule_pk = kwargs.pop('schedule_pk')
+
+                super(StepForm, self).__init__(*args, **kwargs)
+
+                if hasattr(self, 'schedule_pk'):
+                    schedule = Schedule.objects.get(pk=self.schedule_pk)
+
+                    if getattr(schedule, 'use_periodization'):
+                        del self.fields['duration']
+                        new_duration_field = forms.ChoiceField(choices=PERIODIZATION_CHOICES)
+                        self.fields['duration'] = new_duration_field
 
         return StepForm
+
+    def get_form_kwargs(self):
+        kwargs = super(StepEditView, self).get_form_kwargs()
+
+        schedule_step_pk = self.kwargs.get('pk')
+        schedule_pk = ScheduleStep.objects.get(pk=schedule_step_pk).schedule_id
+
+        if 'schedule_pk' not in kwargs:
+            kwargs['schedule_pk'] = schedule_pk
+
+        return kwargs
+
+    def form_valid(self, form):
+        '''Set the schedule and the order'''
+        uses_periodization = False
+        schedule_step_pk = self.kwargs.get('pk')
+        schedule_step = ScheduleStep.objects.get(pk=schedule_step_pk)
+        schedule = schedule_step.schedule
+
+        if schedule_step.is_periodized:
+            uses_periodization = True
+
+        if not uses_periodization and form.cleaned_data['duration'] > 25:
+            # normal scheduled steps should not exceed 25 weeks
+            form.add_error('duration', 'Ensure that duration value is equal or less than 25')
+            return super(StepEditView, self).form_invalid(form)
+
+        max_order = schedule.schedulestep_set.all().aggregate(
+            models.Max('order'))
+        form.instance.schedule = schedule
+        form.instance.order = (max_order['order__max'] or 0) + 1
+        return super(StepEditView, self).form_valid(form)
 
     def get_success_url(self):
         return reverse(
